78722362334628144690baffff13759492613633
Kristina Andric <fashionbykriss@gmail.com>
2024-10-31 12:19:07 +0100

    update

--- Frontend/src/pages/najmovi/NajmoviDodaj.jsx
+++ Frontend/src/pages/najmovi/NajmoviDodaj.jsx
@@ -33,8 +33,6 @@ export default function NajmoviDodaj() {
         setZakupci(odgovor);}
     
     
-      
-    
         useEffect(() => {
             dohvatiStanove();
             dohvatiZakupce();
